6bb196e9d2eb52ceccd8709bd71bc277a125661c
Malcolm Jones <malcolmjones@google.com>
2020-06-01 19:02:18 +0000

    Update code to  fetch greeting message from server and display when clicking button

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -15,14 +15,19 @@
 /**
  * Adds a random greeting to the page.
  */
-function addRandomGreeting() {
-  const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
-
-  // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
-
-  // Add it to the page.
-  const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
+function addGreeting() {
+  const greetingPromise = fetch("/data");
+  greetingPromise.then(handleGreeting);
+  
 }
+
+function handleGreeting(response) {
+  const greetingTextPromise = response.text();
+  greetingTextPromise.then(displayGreeting);
+}
+
+function displayGreeting(greetingText) {
+  const greetingContainer = document.getElementById('greeting-container');
+  greetingContainer.innerText = greetingText;
+}
+
