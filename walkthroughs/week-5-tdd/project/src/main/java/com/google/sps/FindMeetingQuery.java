df7d80104dcbf0ff6c4562fda4cbc4529b159e99
Malcolm Jones <malcolmjones@google.com>
2020-06-18 10:12:43 -0400

    Implement query algorithm

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -14,10 +14,65 @@
 
 package com.google.sps;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+
+    if(request.getDuration() > TimeRange.WHOLE_DAY.duration()) return new ArrayList<TimeRange>();
+
+    ArrayList<TimeRange> availableTimes = new ArrayList<TimeRange>();
+    availableTimes.add(TimeRange.WHOLE_DAY);
+
+    for(Event event : events) {
+
+      boolean eventHasMeetingAttendee = false;
+      for(String eventAttendee : event.getAttendees()) {
+        if(request.getAttendees().contains(eventAttendee)) {
+          eventHasMeetingAttendee = true;
+          break;
+        }
+      }
+      if(!eventHasMeetingAttendee) continue;
+
+      TimeRange eventTime = event.getWhen();
+      for(int i = 0; i < availableTimes.size(); i++) {
+        // if contains else if overlap
+        TimeRange currentAvailableTime = availableTimes.get(i);
+        if(availableTimes.get(i).contains(eventTime)) {
+          //break up interval into two
+          TimeRange splitLower = TimeRange.fromStartEnd(currentAvailableTime.start(), eventTime.start(), false);
+          TimeRange splitUpper = TimeRange.fromStartEnd(eventTime.end(), currentAvailableTime.end(), false);
+          availableTimes.remove(i);
+          if(splitUpper.duration() >= request.getDuration()) availableTimes.add(i, splitUpper);
+          if(splitLower.duration() >= request.getDuration()) availableTimes.add(i, splitLower);
+        }
+        else if(availableTimes.get(i).overlaps(eventTime)) {
+          // check starts and ends and split apppropriately
+          if(eventTime.start() < currentAvailableTime.start()) {
+            TimeRange modifiedAvailableTime = TimeRange.fromStartEnd(eventTime.end(), currentAvailableTime.end(), false);
+            if(modifiedAvailableTime.duration() >= request.getDuration()) {
+              availableTimes.set(i, modifiedAvailableTime);
+            }
+            else {
+              availableTimes.remove(i);
+            }
+          }
+          else if(eventTime.start() > currentAvailableTime.start()) {
+            TimeRange modifiedAvailableTime = TimeRange.fromStartEnd(currentAvailableTime.start(), eventTime.start(), false);
+            if(modifiedAvailableTime.duration() >= request.getDuration()) {
+              availableTimes.set(i, modifiedAvailableTime);
+            }
+            else {
+              availableTimes.remove(i);
+            }
+          }
+        }
+      }
+    }
+
+    return availableTimes;
+    
   }
 }
